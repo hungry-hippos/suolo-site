b2aac29cdbf62fa88a190482c046780ce8f4b5c5
Hungry Hippo <aduca96@gmail.com>
2021-09-23 16:28:15 -0700

    rouogh sketch of second section

--- src/components/SecondSection.js
+++ src/components/SecondSection.js
@@ -1,20 +1,32 @@
 import React from 'react' 
 import TextSlidingUpAnimation from './TextSlidingUpAnimation';
 import TextSlidingLeftAnimation from './TextSlidingLeftAnimation';
-import placeholderImg from '../animations/aNiceSkyline.jpg';
+import planImg from '../images/plan.jpg';
+import constImg from '../images/construccion.jpg'
+import salesImg from '../images/sales.jpg'
 import '../stylesheets/SecondSection.css'
 
 export default function SecondSection(){
         
-        return <div id='secondSection'>
-            <img id='backgroundImage' src={placeholderImg} alt='' />
-            <div id='secondSecLeftH'>
-                <TextSlidingUpAnimation /> 
+        return <div className='secondSection'>
+            <div className='secondSectionEntry'>
+                <TextSlidingUpAnimation type='Planeacion'/> 
+                <div className='entryImgHolder'>
+                    <img src={planImg} alt='' />
+                </div>
             </div>
-            <div id='secondSecRightH'>
-                <TextSlidingLeftAnimation />
+            <div className='secondSectionEntry mirrored'>
+                <TextSlidingUpAnimation type='Construccion'/> 
+                <div className='entryImgHolder'>
+                    <img src={constImg} alt='' />
+                </div>
             </div>
-
+            <div className='secondSectionEntry'>
+                <TextSlidingUpAnimation type='Comercializacion'/> 
+                <div className='entryImgHolder'>
+                    <img src={salesImg} alt='' />
+                </div>
+            </div>
         </div>
     
     
